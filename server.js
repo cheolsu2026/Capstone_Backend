340c574accbf5fa249f873aaca2265687e17da93
san-sae <choi.yoo.lim.12@gmail.com>
2025-09-10 14:11:26 +0900

    [FEAT] 백엔드 포트 변경

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-    console.log('서버 실행 중: http://localhost:${PORT}');
+app.listen(PORT, HOST, () => {
+    console.log(`서버 실행 중: http://${HOST}:${PORT}`);
 });
+
+
