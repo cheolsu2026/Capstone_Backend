ff6428f7e130c806e96d5eafa6d45d058cd3eb0a
chong <chong7750@naver.com>
2025-09-16 11:03:46 +0900

    호스트 추가

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-require('dotenv').config(); 
-
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-    console.log(`서버 실행 중: ${PORT}번 포트`);
+app.listen(PORT, HOST, () => {
+    console.log(`서버 실행 중: http://${HOST}:${PORT}`);
 });
-
-
